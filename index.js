c2c8086bc1caf95b85bf6e20d7c913eef210b643
Ria333 <geraria333@gmail.com>
2021-07-18 11:52:32 +0200

    Part of the homework

---
+++ index.js
@@ -0,0 +1,53 @@
+// створити функцію яка обчислює та повертає площу прямокутника висотою
+// function formulaOfAreaOfRectangle(a, b) {
+//     return (a * b)
+// };
+// let tu = formulaOfAreaOfRectangle(7, 10);
+// console.log(tu);
+
+// - створити функцію яка обчислює та повертає площу кола*/
+// function formulaOfAreaOfCircle (a) {
+//     return (a * 3.14)
+// };
+// let bu = formulaOfAreaOfCircle (5);
+// console.log(bu);
+//
+// - створити функцію яка обчислює та повертає площу циліндру*/
+// function formulaOfAreaOfCylinder (c ,d) {
+//     return (c * d)
+// };
+// let pu = formulaOfAreaOfCylinder(24, 45);
+// console.log(pu);
+//
+
+
+// - створити функцію яка при створює блок з текстом. Текст задати через аргумент
+
+// function SomeText(text){document.write(`<div> ${'skhgdkhsa'} </div>`)};
+// SomeText();
+
+
+// - створити функцію яка при створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+/*function SomeText(text) {
+    document.write(`<div> <ul> <li>${'skhgdkhsa'},</li> <li>${'skhgdkhsa'},</li> <li>${'skhgdkhsa'}</li> </ul></div>`)
+};
+SomeText();*/
+
+// - створити функцію яка при створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+//Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+/*let i=0;
+let text = (`<ul><li>${'skhgdkhsa'}</li></ul>`);
+function SomeText2 (y) {
+    do {i++, document.write(text)}
+    while (i<y) };
+SomeText2(8);*/
+
+
+
+
+
+
+
+
---
+++ trash.js
@@ -0,0 +1,33 @@
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);*/
+//
+let i=0;
+function formula (){
+    if ( i<arguments.length, i++) {
+        return (arguments[i]); console.log (arguments [i])}
+    else if (arguments [i] > arguments.length)
+    {return (arguments [i]); document.write (arguments[i])}
+};
+let a = formula (1,2,3);
+console.log(a);
+
+/*function formula (b){
+
+    for (let i=0;i<a.length; i++) {
+        if (a[i] < a.length);
+        formula=i; {
+            return (formula);}}}}}
+        return (v arguments[i]); console.log (v [i])}
+else if (v [i] > arguments.length)
+    {return (v [i]); document.write (v [i])}
+} };
+let a = formula (1,2,3)
+console.log(a)*/
+
+/*не вышло.как прописать в функции любое количество цифр?*/
+
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+/*function red ([a,'b',c]){
+if (a==="boolean"{}*/
+//*корректно ли писать function nazva ([])?Или как записать что она приняла массив?что значит список?как записывается список?*/
+
